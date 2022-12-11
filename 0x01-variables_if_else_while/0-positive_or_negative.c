b601ba91799b1649519f2bde9a4c870f8521997d
ddatuhung <ddatuhung999@gmail.com>
2022-12-11 14:22:37 -0800

    Correction git commit -m Correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative"
+ * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
 int main(void)
@@ -30,3 +30,4 @@ int main(void)
 	}
 	return (0);
 }
+
